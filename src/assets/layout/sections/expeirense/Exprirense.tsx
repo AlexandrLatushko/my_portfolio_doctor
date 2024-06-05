dff11f0afbc961400ddf0f5b5e7ef37993e0a684
AlexandrLatushko <latushkoalex94@gmail.com>
2024-06-05 16:53:35 +0300

    fixExperience

--- src/assets/layout/sections/expeirense/Exprirense.tsx
+++ src/assets/layout/sections/expeirense/Exprirense.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SectionsTitle } from '../../../../componets/SectionsTitle';
-import { ExprirenseItems } from './exprirenseItems/ExprirenseItems';
-import { FlexWrapper } from '../../../../componets/FlexWrapper';
 import { Container } from '../../../../componets/Container';
+import { theme } from '../../../../styles/Theme';
 
 export const Exprirense = () => {
     return (
         <StyledExprirense>
             <Container>
                 <SectionsTitle>Experience</SectionsTitle>
-                <ExprirenseItems/>
-                <FlexWrapper justify='space-between'>
-                    <ExprirenseDeskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</ExprirenseDeskription>
-                    <ExprirenseDeskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</ExprirenseDeskription>
-                    <ExprirenseDeskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</ExprirenseDeskription>
-                    <ExprirenseDeskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</ExprirenseDeskription>
-                </FlexWrapper>
+                {/* <FlexWrapper justify='space-between'align='center'> */}
+                    <List>
+                        <ListItem>
+                            <Lible>2017</Lible>
+                            <Dot/>
+                            <Deskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</Deskription>
+                        </ListItem>
+
+                        <ListItem>
+                            <Lible>2019</Lible>
+                            <Dot/>
+                            <Deskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</Deskription>
+                        </ListItem>
+
+                        <ListItem>
+                            <Lible>2021</Lible>
+                            <Dot/>
+                            <Deskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</Deskription>
+                        </ListItem>
+
+                        <ListItem>
+                            <Lible>2023</Lible>
+                            <Dot/>
+                            <Deskription>Lorem  dolor sit amet, consectetur adipiscing elit, sed dempor incididunt ut labore et dolore magna adipiscin</Deskription>
+                        </ListItem>
+                    </List>
+                {/* </FlexWrapper> */}
             </Container>
             
 
@@ -29,6 +48,47 @@ const StyledExprirense = styled.section`
 
 `
 
-const ExprirenseDeskription = styled.p`
-    max-width: 260px;
+const List = styled.ul`
+    display: flex;
+
+    justify-content: space-between;
+    align-items: center;
+    gap: 35px;
+
+    position: relative;
+    z-index: 0;
+
+    &::after {
+        content: "";
+        display: inline-block;
+        width: 77%;
+        height: 8px;
+        background: ${theme.color.accent};
+        position: absolute;
+        top: 27px;
+        left: 11%;
+        z-index: -1;
+    }
 `
+
+const ListItem = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+const Lible = styled.span`
+    
+`
+
+const Dot = styled.div`
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background-color: ${theme.color.font};
+`
+const Deskription = styled.p`
+    
+`
+
